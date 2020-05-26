db29cbbdfb2d330d23c3b42dd09be1570b9513b1
Cory Mawhorter <cory.mawhorter@gmail.com>
2020-05-25 21:10:30 -0700

    add bunyan log parsing option

--- cli.js
+++ cli.js
@@ -61,6 +61,11 @@ const { argv } = yargs
       default:    process.env.AWS_REGION,
       describe:   'Select AWS region',
     },
+    bunyan: {
+      type:       'boolean',
+      default:    false,
+      describe:   'Messages are in bunyan log format',
+    },
   })
   .version()
   .help('h').alias('h', 'help');
@@ -76,6 +81,56 @@ const _exitUponComplete = emitter => {
   emitter.once('done', err => {
     process.exit(0);
   });
+};
+
+const _defaultLogger = (argv, record) => {
+  if (argv.time) {
+    const recordTimestamp = new Date(record.timestamp).toISOString();
+    process.stdout.write(`[${recordTimestamp}] `);
+  }
+  process.stdout.write(record.message);
+  if (argv.eol) {
+    // i think windows notepad can handle this now so no need for os.EOL
+    process.stdout.write('\n');
+  }
+};
+
+const _createBunyanLogger = (argv, logGroupName) => {
+  try {
+    const bunyan = require('bunyan');
+    const PrettyStream = require('bunyan-prettystream');
+    const prettyStdOut = new PrettyStream();
+    prettyStdOut.pipe(process.stdout);
+    bunyan.createLogger({
+      name: logGroupName,
+      streams: [
+        {
+          level:      'debug',
+          type:       'raw',
+          stream:     prettyStdOut
+        },
+      ],
+    });
+    return record => {
+      const rawMessage = record.message;
+      try {
+        const data = JSON.parse(rawMessage); // make sure it's valid json
+        if ('v' in data && 'msg' in data) { // assume valid
+          prettyStdOut.write(rawMessage);
+        }
+        else {
+          throw new Error('invalid bunyan message');
+        }
+      }
+      catch (err) {
+        console.log(rawMessage);
+      }
+    };
+  }
+  catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 function main(argv) {
@@ -95,6 +150,13 @@ function main(argv) {
     });
   }
   else {
+    let logger;
+    if (argv.bunyan) {
+      logger = _createBunyanLogger(argv, logGroupName);
+    }
+    else {
+      logger = _defaultLogger.bind(null, argv);
+    }
     const retriever = cwtail.createMessagesRetriever(logGroupName, {
       numRecords:     argv.num,
       follow:         argv.follow,
@@ -102,15 +164,7 @@ function main(argv) {
     });
     _exitUponComplete(retriever);
     retriever.on('record', record => {
-      if (argv.time) {
-        const recordTimestamp = new Date(record.timestamp).toISOString();
-        process.stdout.write(`[${recordTimestamp}] `);
-      }
-      process.stdout.write(record.message);
-      if (argv.eol) {
-        // i think windows notepad can handle this now so no need for os.EOL
-        process.stdout.write('\n');
-      }
+      logger(record);
     });
     if (argv.streams) {
       retriever.on('logstream', logStream => {
